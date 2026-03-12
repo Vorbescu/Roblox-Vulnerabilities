1348043f4debb2a80c7b009bbdeebdaccfdcb7ea
Vorbescu <moldoboss27@gmail.com>
2026-03-11 17:31:20 -0700

    Create Postal.lua

---
+++ Postal.lua
@@ -0,0 +1,9 @@
+local gay = game:GetService("Gay")
+if gay then
+  print("Gay Exists W Speed")
+else
+  local newgay = Instance.new("Configuration")
+  newgay.Name = "Gay"
+  newgay.Parent = game
+  print("Sucessfuly Febypassed")
+  -- fake script boiii
