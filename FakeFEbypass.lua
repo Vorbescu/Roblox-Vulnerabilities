35f1bf6f6586c0be6e754e3d762005a22823c11f
Vorbescu <moldoboss27@gmail.com>
2026-03-11 17:37:37 -0700

    Update and rename Postal.lua to FakeFEbypass.lua

---
+++ FakeFEbypass.lua
@@ -0,0 +1,10 @@
+local service = game:FindFirstChild("Gay")
+
+if service then
+	print("Gay Exists W Speed")
+else
+	local newgay = Instance.new("Configuration")
+	newgay.Name = "Gay"
+	newgay.Parent = game
+	print("Sucessfuly Febypassed")
+end
